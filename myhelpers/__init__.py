8cb3c2d862dd7b0f66d50a5d350abac790ea213b
Cristobal Sifon <sifon@astro.princeton.edu>
2018-01-04 21:37:19 -0500

    renamed to myhelpers

--- myhelpers/__init__.py
+++ myhelpers/__init__.py
@@ -1,7 +1,6 @@
 """Package installation and setup helpers"""
 from __future__ import absolute_import, print_function
 
-from .setup_helpers import *
-
+from .myhelpers import *
 
 __version__ = '0.0.1'
